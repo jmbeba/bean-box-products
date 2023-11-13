9e224cf653396b3f0541ed7a8712622b3a33a738
John Johnson <jjmbeba@gmail.com>
2023-11-13 18:02:03 +0300

    feat: revert index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { create, router as _router, defaults } from "json-server";
-const server = create();
-const router = _router("db.json");
-const middlewares = defaults();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 server.use(middlewares);
 server.use(router);
